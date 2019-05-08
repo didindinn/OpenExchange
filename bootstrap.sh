ce991573b034ee1e7d3cefc11d331b8af49917c8
Talha Buğra Bulut <talhabulut@hotmail.com>
2019-05-08 18:42:48 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,19 +7,19 @@ wget https://bitcoin.org/bin/insecure/bitcoin-core-0.9.2.1/bitcoin-0.9.2.1-linux
 wget https://download.litecoin.org/litecoin-0.8.7.2/linux/litecoin-0.8.7.2-linux.tar.xz
 tar -zxf *.tar.gz
 tar -xf *.tar.xz
-mkdir /home/root/.bitcoin
-mkdir /home/root/.litecoin
-echo "rpcuser=litecoinrpc" > /home/root/.litecoin/litecoin.conf
-echo "rpcpassword=2UY1hkEMoaHrqF7SkmfkziN9HnANfNNz2uUBicbqV1wD" >> /home/root/.litecoin/litecoin.conf
-echo "rpcport=7332" >> /home/root/.litecoin/litecoin.conf
-echo "server=1" >> /home/root/.litecoin/litecoin.conf
+mkdir /root/.bitcoin
+mkdir /root/.litecoin
+echo "rpcuser=litecoinrpc" > /root/.litecoin/litecoin.conf
+echo "rpcpassword=2UY1hkEMoaHrqF7SkmfkziN9HnANfNNz2uUBicbqV1wD" >> /root/.litecoin/litecoin.conf
+echo "rpcport=7332" >> /root/.litecoin/litecoin.conf
+echo "server=1" >> /root/.litecoin/litecoin.conf
 
-echo "rpcuser=bitcoinrpc" > /home/root/.bitcoin/bitcoin.conf
-echo "rpcpassword=94CpFcoCgO" >> /home/root/.bitcoin/bitcoin.conf
-echo "rpcport=8332" >> /home/root/.bitcoin/bitcoin.conf
-echo "server=1" >> /home/root/.bitcoin/bitcoin.conf
-sudo /home/root/bitcoin-0.9.2.1-linux/bin/32/bitcoind -daemon -testnet
+echo "rpcuser=bitcoinrpc" > /root/.bitcoin/bitcoin.conf
+echo "rpcpassword=94CpFcoCgO" >> /root/.bitcoin/bitcoin.conf
+echo "rpcport=8332" >> /root/.bitcoin/bitcoin.conf
+echo "server=1" >> /root/.bitcoin/bitcoin.conf
+sudo /root/bitcoin-0.9.2.1-linux/bin/32/bitcoind -daemon -testnet
 echo
-sudo /home/root/litecoin-0.8.7.2-linux/bin/32/litecoind -daemon -testnet
+sudo /root/litecoin-0.8.7.2-linux/bin/32/litecoind -daemon -testnet
 echo
 sudo redis-server /etc/redis/redis.conf
